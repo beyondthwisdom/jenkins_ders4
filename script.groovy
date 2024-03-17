7a7ae2d2d1a04759be7346d946724cab2f3aa12d
beyondthwisdom <139395290+beyondthwisdom@users.noreply.github.com>
2024-03-17 15:17:31 +0000

    script.groovy updated

--- script.groovy
+++ script.groovy
@@ -1,13 +1,13 @@
 // script.groovy
 def testBuildAndPushImage(String profile, String dockerHubRepo, String version, int buildNumber) {
     def imageTag = "${dockerHubRepo}:${version}-${buildNumber}"
-    sh "./mvnw package jib:build -Djib.to.image=${imageTag} -DskipTests"
+    sh "./mvnw package jib:build -DskipTests -Djib.to.image=${imageTag}"
 }
 
 def prodBuildAndPushImage(String profile, String dockerHubRepo, String version, int buildNumber) {
     def mavenProfile = profile ? "-P${profile}" : ""
     def imageTag = "${dockerHubRepo}:${version}-${buildNumber}"
-    sh "./mvnw package ${mavenProfile} jib:build -Djib.to.image=${imageTag} -DskipTests"
+    sh "./mvnw package ${mavenProfile} jib:build -DskipTests -Djib.to.image=${imageTag}"
 }
 
 return this
